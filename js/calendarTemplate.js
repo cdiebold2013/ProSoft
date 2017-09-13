ac3f4a652c00f96262bad0091caf10e9a9e0ded9
Christine Diebold <cdiebold2013@users.noreply.github.com>
2017-09-13 19:21:08 -0400

    updates

--- js/calendarTemplate.js
+++ js/calendarTemplate.js
@@ -5,13 +5,19 @@ var calendarTemplate = {
   loadTemplate: function () {
     var data = {
       "day": "1",
-      "month":"September"
+      "month":"September",
+      "year":"2017",
+      "days": ["1","2","3","4","5","6","7"]
     },
     template = $("#calendarTemplate").html(),
     output = Mustache.render(template, data);
 
-    console.log("here");
-
     $(".calendarView").append(output);
   }
-}
+};
+
+$(document).ready(function (){
+ //calendarTemplate.init();
+
+ $(".submit").on("click", calendarTemplate.loadTemplate);
+});
