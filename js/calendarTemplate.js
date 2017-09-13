d0e91978ac449836d82031e319760eebfe5ecbb5
Christine Diebold <cdiebold2013@users.noreply.github.com>
2017-09-13 18:49:43 -0400

    restarting plus adding some js

--- js/calendarTemplate.js
+++ js/calendarTemplate.js
@@ -1,7 +1,7 @@
 var calendarTemplate = {
-  init: funciton () {
-      //put js here that should run on page load
-  },
+  // init: funciton () {
+  //     //put js here that should run on page load
+  // },
   loadTemplate: function () {
     var data = {
       "day": "1",
@@ -12,6 +12,6 @@ var calendarTemplate = {
 
     console.log("here");
 
-    $("#templateHere").append(output);
+    $(".calendarView").append(output);
   }
 }
